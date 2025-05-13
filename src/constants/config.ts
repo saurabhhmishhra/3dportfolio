4c87d924f0d7ddadbb710ecff8d4fc6ac9be1990
saurabhhmishhra <saurabhm083@gmail.com>
2025-05-13 22:18:28 +0530

    first commit

---
+++ src/constants/config.ts
@@ -0,0 +1,90 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Saurabh Mishra",
+    fullName: "Saurabh Mishra",
+    email: "saurabhm083@gmail.com",
+  },
+  hero: {
+    name: "SA'M",
+    p: ["I turn ideas into code,", "and code into solutions."],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `I'm a skilled software developer with hands-on experience in TypeScript and JavaScript, and deep expertise in frameworks like React, Node.js, and Three.js. I’m a strong problem-solver with a solid grasp of Data Structures and Algorithms, constantly sharpening my skills to write clean, efficient code. I learn fast, adapt quickly, and collaborate closely with clients and teams to build scalable, user-centric solutions that solve real-world problems. Let’s build something impactful together.`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
